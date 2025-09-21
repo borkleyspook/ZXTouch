79eccfef3c0868173225eb8d76d0fd132941b36e
Borkleyspook <115428985+borkleyspook@users.noreply.github.com>
2025-09-21 12:18:28 -0700

    Redo entitlements and signing

--- common.mk
+++ common.mk
@@ -5,3 +5,15 @@ TARGET = iphone:clang:latest:15.0
 # Add to your Makefile
 DEBUG = 1
 STRIP = 0
+# Use ldid instead of codesign (essential for jailbreak tweaks)
+TARGET_CODESIGN = ldid
+
+# Set default signing flags
+TARGET_CODESIGN_FLAGS = -S
+
+# For rootless, use entitlements
+ifeq ($(THEOS_PACKAGE_SCHEME),rootless)
+    TARGET_CODESIGN_FLAGS = -Sentitlements.rootless.xml
+else
+    TARGET_CODESIGN_FLAGS = -S
+endif
