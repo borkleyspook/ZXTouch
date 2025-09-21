093306b4eef89afa96c73f949c85fea72bda9138
Borkleyspook <115428985+borkleyspook@users.noreply.github.com>
2025-09-21 13:45:57 -0700

    Attempt to fix with code signing
    Combine "Install Modern GNU Make" with "Install Dependencies"
    Add steps to debug entitlements, Verify Binaries and Theos Signing Configuration, Verify Final Binaries
    Remove libhooker

--- common.mk
+++ common.mk
@@ -5,3 +5,12 @@ TARGET = iphone:clang:latest:15.0
 # Add to your Makefile
 DEBUG = 1
 STRIP = 0
+TARGET_CODESIGN = ldid
+# Set default signing flags
+TARGET_CODESIGN_FLAGS = -S
+# For rootless, use entitlements
+ifeq ($(THEOS_PACKAGE_SCHEME),rootless)
+    TARGET_CODESIGN_FLAGS = -S$(THEOS_PROJECT_DIR)/entitlements.rootless.xml
+else
+    TARGET_CODESIGN_FLAGS = -S
+endif
