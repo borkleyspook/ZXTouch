720f23dce6fdadff0a6b1abdb4589114f4663bc0
Borkleyspook <115428985+borkleyspook@users.noreply.github.com>
2026-04-24 09:51:54 -0700

    Update common.mk

--- common.mk
+++ common.mk
@@ -10,7 +10,7 @@ TARGET_CODESIGN = ldid
 TARGET_CODESIGN_FLAGS = -S
 # common.mk
 export ROOT_PATH_NS = /var/jb
-export TARGET_CFLAGS += -DROOT_PATH_NS='@"$(ROOT_PATH_NS)"'
+export TARGET_CFLAGS += -DROOT_PATH_NS='@"$(ROOT_PATH_NS)"' -fobjc-arc
 # For rootless, use entitlements
 ifeq ($(THEOS_PACKAGE_SCHEME),rootless)
     TARGET_CODESIGN_FLAGS = -S$(THEOS_PROJECT_DIR)/entitlements.rootless.xml
