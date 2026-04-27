e77f9ec7328a5dc7cac5f5bc30e59e09148a559c
Borkleyspook <115428985+borkleyspook@users.noreply.github.com>
2026-04-27 14:08:44 -0700

    Revert "Codesign switch test"
    This reverts commit cd9ca4c0ac491cc8c02491b3fc48f022d6d69e98.

--- common.mk
+++ common.mk
@@ -5,7 +5,7 @@ TARGET = iphone:clang:latest:15.0
 # Add to your Makefile
 DEBUG = 1
 STRIP = 0
-TARGET_CODESIGN = codesign
+TARGET_CODESIGN = ldid
 # Set default signing flags
 TARGET_CODESIGN_FLAGS = -S
 # common.mk
@@ -13,13 +13,10 @@ export ROOT_PATH_NS = /var/jb
 export TARGET_CFLAGS += -DROOT_PATH_NS='@"$(ROOT_PATH_NS)"'
 # For rootless, use entitlements
 ifeq ($(THEOS_PACKAGE_SCHEME),rootless)
-	TARGET_CODESIGN_FLAGS = --force --sign - --entitlements $(THEOS_PROJECT_DIR)/entitlements.rootless.xml
+    TARGET_CODESIGN_FLAGS = -S$(THEOS_PROJECT_DIR)/entitlements.rootless.xml
 else
     TARGET_CODESIGN_FLAGS = -S
 endif
-# Use macOS codesign instead of ldid
-
-# Also set component-specific flags (redundant but safe)
-zxtouchb_CODESIGN_FLAGS = --force --sign - --entitlements $(THEOS_PROJECT_DIR)/entitlements.rootless.xml
-appdelegate_CODESIGN_FLAGS = --force --sign - --entitlements $(THEOS_PROJECT_DIR)/entitlements.rootless.xml
-pccontrol_CODESIGN_FLAGS = --force --sign - --entitlements $(THEOS_PROJECT_DIR)/entitlements.rootless.xml
+zxtouchb_CODESIGN_FLAGS = -S$(THEOS_PROJECT_DIR)/entitlements.rootless.xml
+appdelegate_CODESIGN_FLAGS = -S$(THEOS_PROJECT_DIR)/entitlements.rootless.xml
+pccontrol_CODESIGN_FLAGS = -S$(THEOS_PROJECT_DIR)/entitlements.rootless.xml
